072c839431f4cc702b84fbce460a3250834d7031
Hakan Hardal <hakanhardal@icloud.com>
2024-05-06 13:26:34 +0300

    Initial Commit

---
+++ UniversityInformation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniversityInformation
+//
+//  Created by Hakan Hardal on 6.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
